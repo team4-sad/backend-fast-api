29eda1178f36d4af29f979b3ace0506a12d9418b
Schr0dinger <koryazov.dima@gmail.com>
2025-10-14 21:49:05 +0300

    added news endpoint and INewsService

--- src/app.py
+++ src/app.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from src.exceptions.code_exception import CodeException
 from src.handlers import test
+from src.handlers import news
 
 app = FastAPI()
 
 app.include_router(test.router)
+app.include_router(news.router)
+
+
+@app.exception_handler(CodeException)
+async def unicorn_exception_handler(request: Request, exc: CodeException):
+    return JSONResponse(
+        status_code=exc.error_code,
+        content=exc.message,
+    )
