118c3fe68effcdae0d351c5def839790a7d7d392
Artemii Strukov <kavrese@gmail.com>
2025-10-14 23:25:09 +0300

    disable verify

--- src/repositories/news_repository.py
+++ src/repositories/news_repository.py
@@ -11,11 +11,11 @@ class NewsRepository(INewsRepository):
     def get_news_list(self, page: int) -> str:
         if page <= 0:
             raise ValueError("page is unavailable range (<=0)")
-        news_list_page = requests.get(f"{self.base_news_list_url}{page}")
+        news_list_page = requests.get(f"{self.base_news_list_url}{page}", verify=False)
         news_list_page.raise_for_status()
         return news_list_page.text
 
     def get_singular_news(self, news_id: int) -> str:
-        singular_news_page = requests.get(f"{self.base_singular_news}/{news_id}/")
+        singular_news_page = requests.get(f"{self.base_singular_news}/{news_id}/", verify=False)
         singular_news_page.raise_for_status()
         return singular_news_page.text
