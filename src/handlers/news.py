05180f4095822e2de7c5670cd00c6b4d89bfebf2
Schr0dinger <koryazov.dima@gmail.com>
2025-10-24 00:46:29 +0300

    added def in news_service.py

--- src/handlers/news.py
+++ src/handlers/news.py
@@ -21,7 +21,7 @@ async def get_news_list(page: int = 1):
 
 
 @router.get(
-    "/{news_id}",
+    "/item/{news_id}",
     tags=["news"],
     responses={
         200: {"model": SingularNewsModel, "description": "Одна новость"},
@@ -31,3 +31,15 @@ async def get_news_list(page: int = 1):
 async def get_singular_news(news_id: int):
     singular_news = loader.news_service.get_singular_news(news_id=news_id)
     return singular_news
+
+@router.get(
+    "/search",
+    tags=["news"],
+    responses={
+        200: {"model": NewsListResponseModel, "description": "Найденные новости с пагинацией"},
+        503: {"model": str, "description": "Ошибка поиска"},
+    },
+)
+async def get_search_list(search_text: str, page: int = 1, ):
+    searched_news_list = loader.news_service.search_news(search_text=search_text, page=page)
+    return searched_news_list
--- src/loader.py
+++ src/loader.py
@@ -2,6 +2,8 @@ from src.config.config import Config
 from src.database.sqlite_database import SQLiteDatabase
 from src.database.sqls import CREATE_TABLE_NEWS_SQL
 from src.parsers.news_parser import NewsParser
+from src.repositories.db_news_repository import DbNewsRepository
+from src.repositories.migration_news_repository import MigrationNewsRepository
 from src.repositories.news_repository import NewsRepository
 from src.services.news_service import NewsService
 
@@ -11,7 +13,7 @@ database = SQLiteDatabase(config.DATABASE_PATH)
 database.connect()
 
 if not database.table_exists("news"):
-    database.execute_query(CREATE_TABLE_NEWS_SQL)
+    database.execute_script(CREATE_TABLE_NEWS_SQL)
 
 news_repository = NewsRepository(
     base_singular_news_url=config.BASE_SINGULAR_NEWS,
@@ -22,5 +24,7 @@ news_parser = NewsParser(base_link_url=config.BASE_LINK_URL)
 
 news_service = NewsService(
     news_parser=news_parser,
-    news_repository=news_repository
+    news_repository=news_repository,
+    migration_news_repository=MigrationNewsRepository(db=database),
+    db_news_repository=DbNewsRepository(database)
 )
