9d07f058a83fa2214c402f0bc2afc6243000eefc
Artemii Strukov <kavrese@gmail.com>
2025-10-24 16:50:31 +0300

    fixed migration

--- migration.py
+++ migration.py
@@ -5,6 +5,7 @@ from src.config.config import Config
 from src.database.sqlite_database import SQLiteDatabase
 from src.database.sqls import CREATE_TABLE_NEWS_SQL
 from src.parsers.news_parser import NewsParser
+from src.repositories.db_news_repository import DbNewsRepository
 from src.repositories.migration_news_repository import MigrationNewsRepository
 from src.repositories.news_repository import NewsRepository
 from src.services.news_service import NewsService
@@ -18,7 +19,8 @@ db = SQLiteDatabase()
 news_parser = NewsParser(config.BASE_LINK_URL)
 news_repository = NewsRepository(config.BASE_NEWS_LIST_URL, config.BASE_SINGULAR_NEWS)
 migration_repository = MigrationNewsRepository(db)
-news_service = NewsService(news_parser, news_repository, migration_repository)
+db_news_repository = DbNewsRepository(database=db)
+news_service = NewsService(news_parser, news_repository, migration_repository, db_news_repository)
 
 with db:
     if not db.table_exists("news"):
